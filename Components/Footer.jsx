a4dc8f4c2c13b10120d03302d22c65fd265935e2
fateme faraji <83244530+fatemefaraji@users.noreply.github.com>
2025-03-22 14:53:39 +0330

    change some design

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,16 +1,36 @@
 import { assets } from '@/Assets/assets';
-import Image from 'next/image'; // ایمپورت صحیح
+import Image from 'next/image'; 
 import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
-      <Image src={assets.logo_light} alt="logo-light" width={120} height={40} />
-      <p className='text-sm text-white'>ALL rights reserved. Copyright @Bloggy</p>
-      <div className='flex gap-2'>
-        <Image src={assets.facebook_icon} alt='facebook icon' width={40} height={40} />
-        <Image src={assets.twitter_icon} alt='twitter icon' width={40} height={40} />
-        <Image src={assets.googleplus_icon} alt='google plus icon' width={40} height={40} />
+    <div className='flex justify-center flex-col sm:flex-row bg-indigo-600 py-6 sm:py-8 px-6 sm:px-12 items-center'>
+      <Image src={assets.logo_light} alt="logo-light" width={120} height={40} className="mb-4 sm:mb-0" />
+      <p className='text-sm text-white font-medium sm:mx-4'>
+        All rights reserved. Copyright @Bloggy
+      </p>
+      <div className='flex gap-4 mt-4 sm:mt-0'>
+        <Image 
+          src={assets.facebook_icon} 
+          alt='facebook icon' 
+          width={40} 
+          height={40} 
+          className="cursor-pointer hover:scale-110 transition-transform duration-300"
+        />
+        <Image 
+          src={assets.twitter_icon} 
+          alt='twitter icon' 
+          width={40} 
+          height={40} 
+          className="cursor-pointer hover:scale-110 transition-transform duration-300"
+        />
+        <Image 
+          src={assets.googleplus_icon} 
+          alt='google plus icon' 
+          width={40} 
+          height={40} 
+          className="cursor-pointer hover:scale-110 transition-transform duration-300"
+        />
       </div>
     </div>
   );
